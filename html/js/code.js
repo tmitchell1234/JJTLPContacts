5fe308c1b193d90e17bad241fd18edbe4acd07c9
Jerry He <jerryhe365@gmail.com>
2024-01-26 15:09:13 -0500

    Fix bug in table code

--- html/js/code.js
+++ html/js/code.js
@@ -213,11 +213,13 @@ function convertJSONtoTable(data) {
     td.innerHTML = `<button class="btn btn-info">
     <span class="glyphicon glyphicon-edit"></span>
   </button>`;
+    tr.appendChild(td);
 
     td = document.createElement("td");
     td.innerHTML = `<button class="btn btn-danger">
     <span class="glyphicon glyphicon-trash"></span>
   </button>`;
+    tr.appendChild(td);
 
     tableBody.appendChild(tr);
   });

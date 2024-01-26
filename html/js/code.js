c5a17dd3c90da330a768c27e12fa29648324513b
Jerry He <jerryhe365@gmail.com>
2024-01-26 14:23:14 -0500

    Clear Modal on Close

--- html/js/code.js
+++ html/js/code.js
@@ -110,6 +110,16 @@ function doLogout() {
   window.location.href = "index.html";
 }
 
+$("#addContacts").on("hidden.bs.modal", function (e) {
+  $(this)
+    .find("input,textarea,select")
+    .val("")
+    .end()
+    .find("input[type=checkbox], input[type=radio]")
+    .prop("checked", "")
+    .end();
+});
+
 function addContact() {
   let firstName = document.getElementById("firstName").value;
   let lastName = document.getElementById("lastName").value;

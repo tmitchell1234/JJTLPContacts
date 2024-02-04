62d6e35bd95c57d348d0a70cdb2acc5bdd86c569
Jerry He <jerryhe365@gmail.com>
2024-02-04 00:52:48 -0500

    Fix form submission bug

--- html/js/code.js
+++ html/js/code.js
@@ -9,12 +9,6 @@ let srch = "";
 
 let oldData = {};
 
-var form = document.getElementById("addForm");
-function handleForm(event) {
-  event.preventDefault();
-}
-form.addEventListener("submit", handleForm);
-
 function doLogin() {
   userId = 0;
   firstName = "";

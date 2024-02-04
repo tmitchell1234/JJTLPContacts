1cd49c338eaf1c217a3add52518a259ee32507c6
LoganWitte <loganswitte@gmail.com>
2024-02-03 23:15:37 -0500

    Update code.js

--- html/js/code.js
+++ html/js/code.js
@@ -21,7 +21,7 @@ function doLogin() {
 
   document.getElementById("loginResult").innerHTML = "";
 
-  var tmp = { login: login, password: hash };
+  var tmp = { login: login, password: hash , firstName: "", lastName: ""};
 
   let jsonPayload = JSON.stringify(tmp);
 
